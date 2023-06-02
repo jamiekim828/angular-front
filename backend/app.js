688fd2cc813b5eb30eef418823c114adb6e613a5
jamiekim828 <jamiekim828@gmail.com>
2023-06-02 14:38:14 +0200

    successfully getting json

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,29 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  // you will see this on the browser locallhost:8000
-  res.send("Hello from express :)");
-  next();
+// app.use((req, res, next) => {
+//   // you will see this on the browser locallhost:8000
+//   res.send("Hello from express :)");
+//   next();
+// });
+
+app.use("/api/post", (req, res, next) => {
+  const posts = [
+    {
+      id: "randomId1",
+      title: "server-side post1",
+      content: "this is from the server1-1",
+    },
+    {
+      id: "randomId2",
+      title: "server-side post2",
+      content: "this is from the server1-2",
+    },
+  ];
+  res.status(200).json({
+    message: "Post fetched successfully",
+    posts: posts,
+  });
 });
 
 // app.use(express.json());
